fb39fce5ab849e59803865fceb9b6b7ff2989964
Katie Hoffman <ktahoffman@gmail.com>
2014-08-22 10:44:01 -0400

    update sytax for new rspec

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,24 +1,24 @@
-describe 'Anagram' do 
+describe 'Anagram' do
   it 'should detect no matches' do
-    diaper = Anagram.new('diaper')
-    expect(diaper.match(%w(hello world zombies pants dipper))).to eq([])
+    detector = Anagram.new('diaper')
+    expect(detector.match(%w(hello world zombies pants))).to eq([])
   end
 
   it 'should detect a simple anagram' do
     detector = Anagram.new('ba')
-    ba = detector.match(['ab', 'abc', 'bac'])
-    expect(ba).to eq(['ab'])
+    anagrams = detector.match(['ab', 'abc', 'bac'])
+    expect(anagrams).to eq(['ab'])
   end
 
   it 'should detect an anagram' do
     detector = Anagram.new('listen')
-    listen = detector.match %w(enlists google inlets banana)
-    expect(listen).to eq(['inlets'])
+    anagrams = detector.match %w(enlists google inlets banana)
+    expect(anagrams).to eq(['inlets'])
   end
 
   it 'should detect multiple anagrams' do
     detector = Anagram.new('allergy')
-    allergy = detector.match %w(gallery ballerina regally clergy largely leading)
-    expect(allergy).to eq(['gallery', 'regally', 'largely'])
+    anagrams = detector.match %w(gallery ballerina regally clergy largely leading)
+    expect(anagrams).to eq(['gallery', 'regally', 'largely'])
   end
 end
