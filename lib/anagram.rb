d548d12cdb406259333eb66a4aaa8cdb6fd594e1
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-12 13:59:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words) # => array of anagrams, or an empty array
+    matches = words.select { |poss|
+      poss_letters = poss.split("")
+      my_letters = word.split("")
+      poss_letters.sort == my_letters.sort
+    }
+  end
+
+end
