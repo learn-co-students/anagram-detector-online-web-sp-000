3c16660f7bb21969d804cfd56017a8a94e275c4b
David masterson <david73180@gmail.com>
2018-10-14 20:46:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor  :word
+
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(array)
+    new2 = []
+    gram = Anagram.new(word)
+    new = word.split("").sort.join
+    array.each do |x|
+    anag =  x.split("").sort.join
+      if anag == new
+        new2 << x
+        # binding.pry
+      end
+    end
+    return new2
+  end
+
+
+end
