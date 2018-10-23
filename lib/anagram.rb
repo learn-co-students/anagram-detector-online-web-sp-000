118694ae4f318aeb26cd302b21e7cfd4be812d98
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-23 20:07:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def sort_word(word)
+    word.chars.sort
+  end
+
+  def initialize(input)
+    @word = sort_word(input)
+  end
+
+  def match(candidates)
+    anagram_detector = {}
+    candidates.each do |candidate|
+      sorted_candidate = sort_word(candidate)
+      anagram_detector[sorted_candidate] = [] if !anagram_detector.keys.include?(sorted_candidate)
+      anagram_detector[sorted_candidate] << candidate
+    end
+    anagram_detector[word] || []
+  end
+end
