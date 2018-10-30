a3e4d7513d7953a37fe295b87c0dad9fc1da3d02
Rafael Batista <rafaelb24@yahoo.com>
2018-10-30 21:01:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    words.find_all {|word| word.chars.sort.join == @anagram.chars.sort.join}
+  end
+end
