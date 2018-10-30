86857844531ad2e744be086cd18060b93b818f87
Cookie McCormick <muntananuchat@gmail.com>
2018-10-30 12:11:11 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    anagrams = []
+    word_array.each do |word|
+      if word.split("").sort == @word.split("").sort
+        anagrams << word
+      end
+    end
+    anagrams
+  end
+
+end
