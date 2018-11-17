8bccb81e89a7dbf88f6356af42038b253c257746
Nicole D <nnardella@gmail.com>
2018-11-17 20:03:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def match(arr)
+    arr.select do |str|
+      name.split('').sort == str.split('').sort
+    end
+  end
+
+
+end
