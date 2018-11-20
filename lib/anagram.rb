528cf31384ce42a2435ac972a89791217945952f
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-20 16:08:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+   attr_accessor :word
+   
+   def initialize(word)
+    @word = word
+  end
+  
+   def match(collection)
+    output = []
+    collection.each_with_index do |el, idx|
+      if el.split("").sort.join == word.split("").sort.join
+        output << collection[idx]
+      end
+    end
+    output
+  end
+ end 
