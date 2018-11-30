b089627c6aca64c711173e8a5a5aaf9f0c23b4a2
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-30 03:45:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :sentence
+
+  def initialize(sentence)
+    @sentence = sentence
+  end
+
+  def match(string)
+    string.find_all do |word|
+      if word.split("").sort == self.sentence.split("").sort
+        word
+      end
+    end
+  end
+end
+
+
+    #listen = Anagram.new("listen")
+    #listen.match(["enlists", "google", "inlets", "banana"])
+    ##%w(enlists google inlets banana)==["enlists", "google", "inlets", "banana"]
