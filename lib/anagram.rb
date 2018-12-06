2e8ee561a24d803261fbb3f809a5020b5c55c2dd
Jon Mackey <jon@jonmackey.party>
2018-12-06 18:54:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(list)
+    list.find_all{|x| x.split("").sort == word.split("").sort}
+  end
+  
+  def funky_rspec_problem
+    puts "What is this error message about my code looking the same as the lessons?"
+  end
+end
