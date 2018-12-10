555ed016d5d092e7e57d29eac63866a2c4bd72b7
karen ercoli <karenercoli@gmail.com>
2018-12-10 21:19:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,4 +3,8 @@ attr_accessor :word
   def initialize(word)
     @word = word
   end
+  def match(array)
+  array.keep_if {|string| word.split("").sort == string.split("").sort }
+  end
+
 end# Your code goes here!
