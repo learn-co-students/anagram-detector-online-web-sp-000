74ada8111032a658dbd701374ca0f177532118aa
Daniel Dawson <dannyldawson@gmail.com>
2018-12-12 19:55:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagram_array)
+    match_array = []
+    sorted_word = self.word.chars.sort(&:casecmp).join
+    anagram_array.each do |anagram|
+      sorted_anagram = anagram.chars.sort(&:casecmp).join
+      if sorted_word == sorted_anagram
+        match_array << anagram
+      end
+    end
+    match_array
+  end
+end
