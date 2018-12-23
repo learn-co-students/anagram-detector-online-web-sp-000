8e64da2f4fd4881c7dcc62443b26eb9705fe4180
Joe Ganobsik <jganobsik@gmail.com>
2018-12-23 14:47:42 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+    def initialize(word)
+    @word = word 
+  end
+    def match(test_array)
+    valid_matches = []
+    ana_ary = @word.scan(/\w/).sort
+
+    test_array.each do |testcase|
+        test_scanned = testcase.scan(/\w/).sort 
+        if test_scanned ==  ana_ary
+            valid_matches << testcase
+        end
+    end
+
+    return valid_matches
+
+    end
+end
