a2a48c8baeaebc5a5ca8290bbd34ebe50aef26cc
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-29 17:28:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+   attr_accessor :word_to_analyze
+   def initialize(word_to_analyze)
+    @word_to_analyze = word_to_analyze 
+  end 
+  
+  def match(find_anagram_in_this_string) 
+    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    find_anagram_in_this_string.find_all do |word| 
+      if word.split("").sort == self.word_to_analyze.split("").sort 
+        word 
+      end
+    end 
+  end
+  
+end 
+
+
