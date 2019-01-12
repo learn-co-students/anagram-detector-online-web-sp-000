6955e92b89f24277b2e254ada076511cfa7ccce2
Chovin Carlson <Chovin@users.noreply.github.com>
2019-01-12 06:16:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+
+class Anagram
+  attr_accessor :letters
+  
+  def initialize(word)
+   @letters = word.chars.sort
+  end
+  
+  def ==(other)
+    @letters==other.letters
+  end
+  
+  def match(arr)
+    arr.select { |word|
+      Anagram.new(word) == self
+    }
+  end
+end 
