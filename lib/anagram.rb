544e05e7465664f51cea6f97f5915e4ab7882796
J. Singleton <jennifer.singleton@gmail.com>
2019-01-24 01:42:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_array = self.word.split(//).sort
+    array.select do |anagram|
+      word_array == anagram.split(//).sort
+    end
+  end
+end
