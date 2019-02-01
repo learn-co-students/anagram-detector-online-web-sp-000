d7a1de8e4ff9194a0c096cd27ff38d14f6a37e1f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-01 18:10:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(words)
+    words.select {|matching_word| matching_word.chars.sort_by(&:downcase).join == word.chars.sort_by(&:downcase).join  }
+  end
+
+end
