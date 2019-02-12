60c6c50aecd74715b9383ab712b08a1e00125fde
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-02-12 14:34:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,13 +5,19 @@ def initialize(keyword)
 @keyword = keyword
 end
 
-def match(array)
-  @word_array = []
-  array.map do |word|
-    if (word.chars.sort == @keyword.chars.sort)
-    @word_array << word
-    end
-  end
-    @word_array
+#def match(array)
+#  @word_array = []
+#  array.map do |word|
+#    if (word.chars.sort == @keyword.chars.sort)
+#    @word_array << word
+#    end
+#  end
+#    @word_array
+#end
+
+ def match(array)
+array.select {|word| word.split("").sort == @keyword.split("").sort}
 end
+
+
 end
