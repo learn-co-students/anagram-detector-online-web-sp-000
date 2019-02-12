d74745e1f5f43eda2f304928317dbbdbb30e297f
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-02-12 14:28:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+attr_reader :keyword
+
+def initialize(keyword)
+@keyword = keyword
+end
+
+def match(array)
+  @word_array = []
+  array.map do |word|
+    if (word.chars.sort == @keyword.chars.sort)
+    @word_array << word
+    end
+  end
+    @word_array
+end
+end
