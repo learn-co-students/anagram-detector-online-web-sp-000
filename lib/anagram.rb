64cd0db35168953aaf3fcdaf072d902e2157704b
Aglae Verduzco <aglae.cv@gmail.com>
2019-02-17 21:40:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.select do |item|
+      @word.split("").sort) == (item.split("").sort)
+    end
+  end
+
+end
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
