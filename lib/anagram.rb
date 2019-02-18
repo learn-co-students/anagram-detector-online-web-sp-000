a502287ba3004bb134ec90aefea2dd588c846101
Bry Cox <carl.cc@gmail.com>
2019-02-18 23:59:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,5 @@
 # Your code goes here!
+require 'pry'
 class Anagram
   attr_accessor :data
 
@@ -6,9 +7,11 @@ def initialize(data)
   @data = data
 end
 
-def match(data)
-  data
-
+def match(arr)
+ var_2 = []
+ var = data.split("").sort
+ var_1 = arr.collect {|x| x.split("").sort}
+ var_1.each_with_index {|x, i| var_2 << arr[i] if x == var}
+ var_2
 end
-
 end
