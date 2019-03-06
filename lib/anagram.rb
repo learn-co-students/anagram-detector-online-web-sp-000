6519711a1c5aebadf98482b85da3d5b1b734efea
Aaron Thompson <thompson_ad@outlook.com>
2019-03-06 14:29:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+   attr_accessor :word
+
+   def initialize(the_word)
+     @word = the_word
+   end
+
+   def match(some_words)
+     #iterate over some_words and compare to @word
+     #need to split the word into an array of letters and then sort them
+     match_array = []
+     some_words.each do |test_word|
+       alph_test_word = test_word.split("").sort
+       alph_word = @word.split("").sort
+       if alph_test_word == alph_word
+         match_array << test_word
+       end
+     end
+     match_array
+   end
+ end
