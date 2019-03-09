e8d106505ded4239b17b363c4faaf5293890488c
ADIL NAIMI <adilnm23@hotmail.com>
2019-03-09 04:28:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  def initialize(word)
+    @word=word.split("").sort.join
+    @all=[]
+  end
+  
+  def match(array)
+    array.each do |x|
+      arr=x.split("").sort.join
+      @all<< arr
+    end
+    
+    result=@all.find_all{|i| i==@word}
+    if result
+      result.collect do|x| 
+      indx=@all.index(x)
+      @all[indx]=""
+      array[indx]
+  end
+  else
+    []
+  
+  end
+end
+end
