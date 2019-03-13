18612f9163f90c95bfcf291fb145793d219eceba
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-13 20:12:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    # binding.pry
+    word_array.collect do |array_word|
+      @word.split("").sort == array_word.split("").sort ? array_word : nil
+    end.compact
+  end
+
+
+
+end
