d960c063bea94e6f8b4fa7488ab68e1ab699fc1e
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-03-22 12:05:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word, :matches
+
+  def initialize(word)
+    @word = word
+    @matches = []
+  end
+
+  def match(word_array)
+    word_array.select { |w| w.split("").sort == @word.split("").sort }
+  end
+
+end
