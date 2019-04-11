01581b765d7ef5e62c108a0937489444c039118f
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-11 00:41:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  attr_accessor:word 
+  
+  @word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(input)
+    matches_array = []
+    sorted_array = []
+
+       input.each  { |val| sorted_array << val.chars.sort.join }
+       sorted_word = @word.chars.sort.join
+       
+        sorted_array.each_with_index do |val, index|
+          if val == sorted_word 
+            matches_array << input[index]
+          end 
+        end 
+   
+
+    return matches_array
+    end 
+      
+end 
