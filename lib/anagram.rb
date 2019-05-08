393691900b00aca6326b313f1ca956b52d1c45a9
Christopher Luna <chris@illusionlayervfx.com>
2019-05-08 04:27:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_array)
+    word_array.select do |w|
+      word_split = @word.split("")
+      w_split = w.split("")
+      word_split.sort == w_split.sort
+    end
+  end
+
+end
