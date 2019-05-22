96f817aad524f088af4d3ecb80b58420f5b3a950
Alan Krajina <alankrajina88@gmail.com>
2019-05-22 17:55:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,43 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)   # word to track and match
+    @word = word
+  end
+  
+  def match(words)
+    words.filter { |first| first.split("").sort == @word.split("").sort } 
+  end
+  
+end
+
+
+
+=begin
+# collect tocno vraca ["hello", nil, nil, nil, nil]
+#.compact vraca error
+  
+all=[]
+words= ["hello", "world", "zombies", "pants", "dipper"]
+
+word="oellh"
+
+words.collect do |first|    
+
+  if first.split("").sort == word.split("").sort
+  all << first
+  all.join(",")
+    else
+    nil
+  end
+end
+
+
+word="oellh"
+["hello", "world", "zombies", "pants", "dipper"].filter { |first| first.split("").sort == word.split("").sort } 
+
+# VRACA DOBRO
+
+=end
