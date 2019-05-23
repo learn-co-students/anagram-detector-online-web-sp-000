4441e7933505070003edb5084934e89e06e138ae
Alan Krajina <alankrajina88@gmail.com>
2019-05-23 07:13:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,31 +13,3 @@ class Anagram
   
 end
 
-
-
-=begin
-# collect tocno vraca ["hello", nil, nil, nil, nil]
-#.compact vraca error
-  
-all=[]
-words= ["hello", "world", "zombies", "pants", "dipper"]
-
-word="oellh"
-
-words.collect do |first|    
-
-  if first.split("").sort == word.split("").sort
-  all << first
-  all.join(",")
-    else
-    nil
-  end
-end
-
-
-word="oellh"
-["hello", "world", "zombies", "pants", "dipper"].filter { |first| first.split("").sort == word.split("").sort } 
-
-# VRACA DOBRO
-
-=end
