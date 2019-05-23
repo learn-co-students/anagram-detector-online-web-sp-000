5011b20535bbf3423cc5aa74f38b515452a15fa0
elijahmm17 <elijahmm17@gmail.com>
2019-05-23 20:19:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram 
+  attr_reader :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(phrase) 
+    results = []
+    phrase.each do |w| 
+      if @word.chars.sort.join == w.chars.sort.join
+        results << w
+      end 
+    end 
+    results
+  end 
+end 
