94ca8c70e871c132b9acf54c18d00c680987d5b5
Trish Ogen <patriciaogen@gmail.com>
2019-05-29 02:45:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matching_words = []
+    word_array = @word.split("").sort
+    array.each do |word|
+      word_to_match = word.split("").sort
+      matching_words << word if word_array == word_to_match
+    end
+    matching_words
+  end
+
+end
