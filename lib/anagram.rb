27f53084353ea7c480c7f0ff1ad70e79299869e6
Becky Phan <beckyphan@live.com>
2019-06-06 00:50:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select do |word_in_array|
+      word_in_array.split("").sort == self.word.split("").sort
+    end
+  end
+  
+end
