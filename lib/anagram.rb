7c2e36a8516d74d3ef1a8e2fa3c2de067ca303ba
Marc Poppy <poppy.marc@gmail.com>
2019-06-13 04:30:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require 'pry'
+#create class Anagram that takes a word on initalize
+class Anagram
+
+  attr_accessor :word , :split_word, :compare_word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(match_array)
+    match_array.select do |word|
+       word if word.split("").sort == @word.split("").sort
+    end
+  end
+
+end
+#
+
+
+#end result - running the program
+
+listen = Anagram.new("listen")
+listen.match(%w(enilsts google inlets banana))
+# => ["inlets"]
