e5a8115987d20a7c073889ccc3fd0672e45d3f55
Tracy Reuther <tracyreuther@gmail.com>
2019-06-25 22:49:59 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(word_array)
+  word_array.select do |ea_word|
+    ea_word.split("").sort == @word.split("").sort
+  end
+end
+
+end
