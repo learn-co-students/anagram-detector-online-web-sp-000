5ca12edbcbf5003ea3caa8a38c1bf0bfe23fb0a0
Eric Clark <eclark0013@gmail.com>
2019-06-26 13:16:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word, :check_array
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(array)
+    annagrams=[]
+    array.each do |checker|
+      if checker.split("").sort==word.split("").sort
+        annagrams<<checker
+      end
+    end
+    annagrams
+  end
+end
