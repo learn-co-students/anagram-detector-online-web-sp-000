ccc20be60b687a7347bd1471f4eb57a6dc99c7c4
Kimberly Rouse <ksrouse94@gmail.com>
2019-06-26 23:18:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+  
+  anagrams = []
+  
+  #setter and getter for initialized word 
+  attr_accessor :entry
+  
+  def initialize(entry)
+    @entry = entry 
+  end  
+  
+  def match(anagrams)
+    #create empty array to hold matching result(s)
+    result = []
+    #iterate over array of possible anagrams
+    anagrams.each do |word|
+      if @entry.split("").sort == word.split("").sort
+        result << word
+      else
+        result
+      end  
+    end  
+    result
+  end
+end
