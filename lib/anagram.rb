319629b09e0374d5d264fcb89f26ec7a8a6616f6
Shawn <shawntennity@gmail.com>
2019-06-29 17:18:43 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+    agram_letter = @word.split('').sort
+    match_array = []
+
+    words_array.each_with_index do |word, index|
+      word_letter = word.split('').sort
+
+      if agram_letter == word_letter
+        match_array << words_array[index]
+      end
+
+    end
+    match_array
+  end
+
+end
+
+# binding.pry
