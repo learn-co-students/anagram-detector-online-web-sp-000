5910b7c652db946d0e530a0eb3a8a837addc20e3
Connor Cowan <ccowan003@comcast.net>
2019-07-01 20:57:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word            #takes a word on initialization
+  end
+
+  def match
+    @word.collect do |word|
+
+    end
+  end
