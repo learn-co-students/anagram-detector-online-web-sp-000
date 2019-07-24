5c4dc6ff584cf939d8539252eeadf2b18428f64f
Alice Brunel <alc.brunel@gmail.com>
2019-07-24 21:01:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name.split("")
+    @name
+  end
+  
+  def match(array)
+  match = []
+    array.each do |word|
+      if word.split("").sort == name.sort
+        match << word
+      else
+        nil
+      end
+    end
+  match
+  end
+
+end 
