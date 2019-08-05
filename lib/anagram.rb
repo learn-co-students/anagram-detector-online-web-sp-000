43e7235e8641c38c85c2756df3a47f727f2c92d5
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-05 22:02:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    anagrams = []
+    list.each do |element|
+      if element.split("").sort == @word.split("").sort
+        anagrams << element
+      end
+    end
+    anagrams
+  end
+end
