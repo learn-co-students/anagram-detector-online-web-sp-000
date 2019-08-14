69c146cc6e140c16fc3e64fbb0af83881344d664
chic-page-6502 <zacharytq@gmail.com>
2019-08-14 01:38:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    thing = array.select {|c| c.split("").sort == @word.split("").sort}
+    thing
+  end
+
+end
