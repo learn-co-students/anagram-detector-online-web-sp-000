2d584e6ebc598be315bc38e5ab9950cba7a3e327
Marianne Pyon <mariannepyon@gmail.com>
2019-08-20 20:25:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,35 @@
 # Your code goes here!
+#class Anagram
+
+#  attr_accessor :word
+
+#  def initialized(word)
+#    @word = word
+#  end
+
+#  def match(array_of_words)
+#    array_of_words.find_all do |element|
+#      if word.split("").sort == sort.element.split("").sort
+#        word
+#    end
+#  end
+#end
+#end
+
+class Anagram
+  attr_accessor :word_to_analyze
+
+  def initialize(word_to_analyze)
+    @word_to_analyze = word_to_analyze
+  end
+
+  def match(find_anagram_in_this_string)
+    
+    find_anagram_in_this_string.find_all do |word|
+      if word.split("").sort == self.word_to_analyze.split("").sort
+        word
+      end
+    end
+  end
+
+end
