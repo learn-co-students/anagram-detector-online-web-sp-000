a9983dbac9672a25a72ea0294647ef225f29c67f
Zach  Smith <zdsmith001@gmail.com>
2019-08-22 19:46:44 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    sorted_word = @word.split("").sort
+    matches = []
+    words.each do |new_word|
+      sorted_new_word = new_word.split("").sort
+      if sorted_new_word == sorted_word
+        matches << new_word
+      end
+    end
+    matches
+  end
+
+end
