9796ab3548a737bb155acc97a4d26e5c820f1138
Chantel Lowe <chantellowe17@gmail.com>
2019-08-28 10:51:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word_array)
+    word_array.select { |element| @word.split("").sort == element.split("").sort }
+  end
+  
+end
