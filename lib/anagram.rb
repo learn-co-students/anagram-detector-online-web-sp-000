c36d433abcf375805f827c1ac9e80d31a43ec1b4
Christopher Dent <christopherdent01@gmail.com>
2019-08-29 01:19:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,40 @@
 # Your code goes here!
+require 'pry'
+
+
+
+
+class Anagram
+attr_accessor :word  
+
+
+    
+
+  def initialize(word)
+    
+    @word = word.split("").sort 
+    @results = []
+  end 
+  
+  def match(array)
+    
+    
+    array.each do |w|
+      
+      
+ 
+      if w.split("").sort != @word
+        @results
+        
+      else 
+        @results << w
+      end 
+      
+    end 
+ 
+    @results 
+  end 
+  
+
+  
+end 
