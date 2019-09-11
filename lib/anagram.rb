af892372a2c3ae8925c1bbe60cba5c04ce8002a2
Michael Lynch <michael@michaelrlynch.com>
2019-09-11 21:35:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select { |x| x.split("").sort == @word.split("").sort }
+  end  
+  
+  
+  
+end
