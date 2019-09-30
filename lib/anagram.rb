9bdc224fd713400beee93ca5cb3fbae96c36f340
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-30 22:58:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |item|
+      @word.split("").sort == item.split("").sort
+    end
+  end
+
+end
