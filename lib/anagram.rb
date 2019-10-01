d27a69651bdd3c636c5b32d4fe8eed817c94ee33
Adam Weissman <adam@comedytragedyepic.com>
2019-10-01 08:38:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   end   
 
   def match(array)
-    array.select {|x| x.split("").sort == @name.split("").sort}
+    array.select {|x| x.chars.sort == @name.chars.sort}
   end   
 
 end 
