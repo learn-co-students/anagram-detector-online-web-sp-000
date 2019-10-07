27b05d12d95a1a7f4518f6d6a18c0514f9f77c7e
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-10-07 15:16:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  
+  attr_accessor :special_word
+  
+  
+  def initialize(special_word)
+    @special_word = special_word.split("")
+  end
+  
+  def match(words)
+    answer_array = []
+    words.each do |x| 
+      if @special_word.sort == x.split("").sort
+        answer_array << x
+  end
+end
+answer_array
+end
+  
+
+end
