92246de257cc4b940232fc412226da81c6640c3f
Jack OMalley <jfo.omalley@gmail.com>
2019-10-11 18:33:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :key_word
+
+  def initialize(key_word)
+    @word = key_word
+  end
+
+  def match(array)
+    anagram = @word.split("").sort!
+    array.select {|item| anagram == item.split("").sort}
+  end
+
+  #end of class
+end
