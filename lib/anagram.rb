4c9d825e5855cb4751255f9297bdc218215b68b2
Christopher Dent <christopherdent01@gmail.com>
2019-10-20 22:27:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,40 +1,30 @@
-# Your code goes here!
 require 'pry'
 
-
-
-
 class Anagram
-attr_accessor :word  
-
-
-    
+  attr_accessor :word
 
   def initialize(word)
-    
-    @word = word.split("").sort 
-    @results = []
+    @word = word 
   end 
+  
   
   def match(array)
     
+    @matches = [] 
+    @anagram = @word.split("")
     
-    array.each do |w|
-      
-      
- 
-      if w.split("").sort != @word
-        @results
-        
-      else 
-        @results << w
-      end 
-      
-    end 
- 
-    @results 
+    
+    array.each do |item| 
+      @match = item.split("")
+      if  @match.sort == @anagram.sort
+        @matches << @match.join
+      end
   end 
-  
-
+    @matches 
+ end   
+    #detector = Anagram.new('ba')
+    
+    
+   
   
 end 
