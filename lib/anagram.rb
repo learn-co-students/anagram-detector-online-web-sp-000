6a8f66dfc34876928c865a64975366a9d8c1c2e7
Jazmin M <jazminmerckel@gmail.com>
2019-10-25 00:28:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(possible_anagrams)
+    anagrams = []
+    possible_anagrams.each do |possible_anagram|
+      if anagram_match(word, possible_anagram)
+        anagrams << possible_anagram
+      end
+    end
+    return anagrams
+  end 
+  
+  def anagram_match(string1, string2)
+    string1.split("").sort == string2.split("").sort
+  end
+  
+end
