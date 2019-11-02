a74dd385b330da759594f8fd1d23d643bae0e2e7
Haim D <haim11293@aol.com>
2019-11-02 08:03:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+# match method takes a parameter of words_array and splits each letter of the word then sorts the array, then it compares that array to the @word array that has also been split and sorted to see if the word has any matching letters.
+  def match(words_array)
+    words_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
