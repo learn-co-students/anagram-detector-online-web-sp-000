9f3ef0c0aab8e093db18b58d8729948ff1b42cea
wei chen <weichen9083@gmail.com>
2019-11-03 19:04:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+
+
+class Anagram 
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    
+    @word = word 
+  
+  end 
+  
+  def match(array)
+    y = @word.split("").sort
+    z = []
+    array.each do |word|
+      if word.split("").sort == y 
+        z << word
+      end 
+    end 
+    z
+  end 
+  
+end 
+
+
