4c89319d44b976533741676c03fe696f222ef4fb
Bruce Ruffin <bruffin1221@gmail.com>
2019-11-10 14:21:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :words
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(word)
+    word.select{|x| (x.split("").sort==@word.split("").sort)}
+  end
+
+ end
