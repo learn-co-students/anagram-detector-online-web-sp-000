1eaca3c66bf41e2ef24fd83af1ab308a9b368f25
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-11-17 12:51:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word) 
+    @word = word 
+  end 
+  
+  def match(array)
+    array.select {|element| element.split("").sort == word.split("").sort}
+  end   
+end   
+
+
