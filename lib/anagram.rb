b557600a0ad17451b6b379834acd9953049dbc29
Riley Slayden <slaydenriley@gmail.com>
2019-11-19 17:14:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  
+  def match(string)
+    array = []
+    string.select do |words|
+      words.split('').sort.join == @word.split('').sort.join
+    end
+  end
+end
+
