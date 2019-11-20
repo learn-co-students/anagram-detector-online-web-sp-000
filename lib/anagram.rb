2fa1ed6e5ce94dd7e5f2c2900a45bfff4796cbef
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-20 08:03:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(word)
+  #  binding.pry
+  word.keep_if { |w|  w.split('').sort == words.split("").sort }
+#keep instance word if (iterate over individual words and split letters, then sort)
+# is equal to (instance words split, and sorted)
+  end
+end
