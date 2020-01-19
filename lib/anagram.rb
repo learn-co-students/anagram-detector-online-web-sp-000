be2d63b799e3bfe1e71116fcb84a17a9b300e69e
Jonathan Koss <jonathan_koss@yahoo.com>
2020-01-19 03:50:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,30 +7,11 @@ class Anagram
   
   def initialize(word)
     @word = word 
-    # Create a .match method
-    # The argument is an array of possible anagrams
-    def self.match(psbl_anagrams)
-      # I need to return an array of an anagram or multiple anagrams
-      # The class should detect if there no matches
-      # It should return an empty array 
-      # I want to iterate through the array to see if the word arg mataches any of the words
-      # I want to split word into an array and then sort the arg and the element in the arr to see if they match
-      # After I find that the new word and element and the elements matches, I want to push that word into the new array.
-      # After all of the elements are pushed into the arr, I want the new array returned
-      # If the word does not match the elements, return an empty array 
-      anagrams = []
-      psbl_anagrams.each do |psbl_anagram|
-        if psbl_anagram.split("").sort == @word.split("").sort
-          anagrams << psbl_anagram 
-        end
-      end 
-      
-     
-      # Anagram should detect a simple anagram
-      # The class will detect an anagram 
-      # The class will detect multiple anagrams 
-      
-    end 
-    
+  end
+  
+  def match(psbl_anagrams)
+    psbl_anagrams.select do |psbl_anagram| 
+      psbl_anagram.split("").sort == @word.split("").sort
+    end
   end 
 end 
