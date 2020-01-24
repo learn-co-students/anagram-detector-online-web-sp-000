eaf764f0f7ef1f5c149b7a3c66ef9c983728e2d7
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-24 23:40:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+def initialize(anagram)
+  @anagram = anagram
+end
+  
+def match(array)
+  #iterate thru array to compare with anagram
+  #split both words (one from array + anagram) into individual letters
+  #sort letters and compare to anagram
+  #return (all) match(es)
+  array.select do |word| 
+    word.split("").sort == @anagram.split("").sort
+  end
+end
+  
+end
