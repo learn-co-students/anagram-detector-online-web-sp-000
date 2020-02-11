623a5a1515e7465994b6f5df85e6732a2b3421e2
Aoune Syed <aoune007@gmail.com>
2020-02-11 05:32:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :finding_anagrams
+
+   def initialize(word)
+     @finding_anagrams = word
+   end
+
+   def match(array)
+     array.select {|x| x.split("").sort == @finding_anagrams.split("").sort}
+   end
+
+
+end
