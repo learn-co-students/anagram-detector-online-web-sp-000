fa01399531132735316d2abb8af286287d978404
Sarah Larkworthy <sarah.larkworthy@gmail.com>
2020-02-14 15:22:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+    
+  end
+  
+  def match(possibilities_array)
+    word_letters = @word.split("").sort
+    matched_word = []
+    possibilities_array.each do |word|
+      matched_word << word if (word_letters == word.split("").sort)
+    end
+    matched_word
+  end
+  
+end
