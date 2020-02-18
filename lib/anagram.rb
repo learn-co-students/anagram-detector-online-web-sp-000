4d9931de596b89065e844c6468333fbed74b71d9
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-18 09:06:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(new_word)
+    @word = new_word
+    
+  end
+  
+  def match(word_array)
+    word_array.select {|words| words.split("").sort == @word.split("").sort}
+  end
+  
+  
+end
