077daa6d38fe92400c8baca3788dcd2201733c7f
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-25 01:15:02 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :word, :possible_matches
+
+def initialize(word)
+@word = word
+end
+
+def match (possible_matches)
+
+  letters_list = @word.split("").sort
+
+@possible_matches = possible_matches
+
+  @possible_matches.select { |words|
+  words.split("").sort == letters_list}
+  end
+
+
+end
