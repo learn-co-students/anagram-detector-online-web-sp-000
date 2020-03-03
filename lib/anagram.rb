6ccdf3337dac4aab462cd24433a49ba19854f5a1
Seth Todd <seth.todd@outlook.com>
2020-03-03 02:44:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    array.select do |words|
+    @word.split("").sort == words.split("").sort
+    end
+  end
+#binding pry
+end
