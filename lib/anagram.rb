b5fcdf5ee6bf0e533b9fa30aaa37727c50eb1c9e
Will Corcoran <corcoranwill@gmail.com>
2020-03-08 11:54:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  # .select returns an array of elements matching a criteria
+  # .split breaks a string into an array containing each letter of original string
+  # .sort orders an array of letters in alpha order 
+  def match(words)
+    if words.select {|word| word.split("").sort == @word.split("").sort} == nil
+    else
+      words.select {|word| word.split("").sort == @word.split("").sort}
+    end
+  end
+end
