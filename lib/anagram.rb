052e60ce7d83e7596acaf8797275ad82c6e3bbfd
Brian Vehige <brianvehige@gmail.com>
2020-03-08 00:40:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+# Your code goes here
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word 
+end
+
+def match(array)
+  array.select do |words|
+    @word.split("").sort == words.split("").sort
+  end
+end
+
+end
