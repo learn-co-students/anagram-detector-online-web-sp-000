d6776a11b948104bc29d9834b73de86085ab8629
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-03-20 15:04:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+# Your code goes here
+
+class Anagram
+  
+  attr_accessor :word
+  
+  
+  def initialize(word)
+    @word = word
+  end
+   
+  def match(array)
+   #take in an array and return all matches in an array, if no matches exist, it should return an empty array.
+  array.select {  |string| @word.split("").sort == string.split("").sort }
+    
+  end
+  
+  
+end
