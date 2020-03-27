448ed7f9d65a35b4d16d14d6b68b72678a3fa225
C. Leow <chienleow1@gmail.com>
2020-03-27 04:15:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,21 +11,4 @@ class Anagram
     # binding.pry
     array.keep_if {|w| word.split("").sort == w.split("").sort}
   end
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  #   array_of_words.each {|w| w.split("")}.sort
-  
-  # # iterate over the array of words that the .match method takes as an argument
-  # # compare each word of that array to the word that the Anagram class is initialized with
-  # # try determining if they are composed of the same letters
-  # # use some_word.split("")
-  # # compare two arrays using .sort
-
 end
