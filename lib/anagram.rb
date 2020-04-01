ea92b881040f0dc50c0e9fc9718750f3aedb794e
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-01 01:26:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  # match method will take in an array of strings
+  def match(word_array)
+    results = []
+    word_array.each do |word|
+      results << word if word.split("").sort == @word.split("").sort
+    end
+    results
+  end
+end
