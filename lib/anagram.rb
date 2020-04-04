4773d2825f0695c02b213494a3fc2b4d85f63467
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-04 12:01:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,20 +9,10 @@ class Anagram
 
   def match(array_of_anagrams)
     #turn the word into an array
-    array_of_letters = @word.split("")
-    array_of_matches = []
     #check if the lengths are the same
-    array_of_anagrams.each do |possible_word|
-      if possible_word.length == @word.length
-          #turn possible_word into an array of letters
-          possible_word_array = possible_word.split("")
-          #see if array_of_letters includes each element of possible word
-        if possible_word_array.sort == array_of_letters.sort
-          array_of_matches << possible_word
-        end
-      end
+    array_of_anagrams.select do |possible_word|
+      possible_word.split("").sort == @word.split("").sort
     end
-    array_of_matches
   end
 
   end
