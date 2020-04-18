ecfbaff207777c3db023460173a6ce5dd5aaaf6d
Elliot Blanchard <public@elliotblanchard.com>
2020-04-18 18:25:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+
+    attr_accessor :search_word
+
+    def initialize(search_word = nil)
+      @search_word = search_word
+    end
+
+    def match(potential_anagrams)
+      anagrams = []
+      potential_anagrams.each do |potential|
+        if @search_word.split("").sort == potential.split("").sort
+          anagrams << potential
+        end
+      end
+      anagrams
+    end
+
+end
