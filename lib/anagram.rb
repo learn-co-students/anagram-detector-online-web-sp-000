c5c2e3165526d62076913995c34fd1c703dd37fd
The-Gentleman <zraja4402@gmail.com>
2020-05-06 21:48:33 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word)
+        @word = word 
+    end
+
+    def match(array_of_words)
+        array_of_words.select do |word|
+            word.split('').sort == @word.split("").sort
+        end 
+    end 
+
+end 
