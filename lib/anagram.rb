c1742fd58048f5268a352701f2067131325ef16b
asb1512 <andrew@olympicmedia.com>
2020-05-09 01:19:12 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    # go through each possible anagram and split into an array or single characters
+    # store a variable with the word to be tested split as an array
+    # use .include? to see if the possible anagram 'e' includes the same letters as the argument word
+    def match(possible_anagrams)
+        anagrams = []
+        split_word = @word.split("")
+        possible_anagrams.each do |possible_anagram|
+            split_possible = possible_anagram.split("")
+            if split_word.sort == split_possible.sort
+                anagrams << possible_anagram
+                # binding.pry
+            end
+        # binding.pry  
+        end
+        anagrams
+    end
+
+end
