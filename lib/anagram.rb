7e3876d4e32f6f78e5b1df91f7c0ae5ff1b1e077
Matt Im <matthew.seiim@gmail.com>
2020-05-10 20:52:31 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+attr_accessor :words
+
+def initialize(words)
+  @words = words
+end
+
+def match(words2)
+  words2.select do |words|
+    if words.split(//).sort == @words.split(//).sort
+      true
+    else
+      false
+    end
+  end
+end
+end
