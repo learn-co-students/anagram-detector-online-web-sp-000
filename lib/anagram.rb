5f66396ebdfcc3600a93e72e4cab2be494f07323
Jake G <jake098877@gmail.com>
2020-05-11 22:27:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    a = []
+    array = []
+    @word = @word.split("")
+    @word.sort!
+    @word = @word.join
+    o = 0
+    list.each_with_index do |i, idx|
+    array = i.split("")
+    array.sort!
+    array = array.join
+
+    if @word == array
+      a << list[idx]
+  end
+    end
+    a
+  end
+
+end
