3f37240ad10fffb3b299c736b06f22fb0248f19b
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-18 04:37:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.find_all do |element|
+      @word.split("").sort.join ==
+      element.split("").sort.join
+    end
+  end
+
+end
