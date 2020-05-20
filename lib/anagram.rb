fb7a4408d66ee173870e7690d85b7b8fd14a7e95
Jung Lee <lee.jung.s95@gmail.com>
2020-05-20 03:27:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams_array)
+    anagrams_array.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+
+end
