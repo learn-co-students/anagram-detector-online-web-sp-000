47523d2c15bac6f7ce11eb74be800826f1ce8e8c
elsa chen <snowing222@gmail.com>
2020-05-29 19:29:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :string
+  def initialize(string)
+    @string=string
+  end
+
+  def match(array)
+    array.select do |element|
+      string.split("").sort==element.split("").sort
+    end
+  end
+
+end
