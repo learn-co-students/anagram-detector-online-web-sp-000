6ce719fe3c52ba5e93906e6121371962e8ead1a6
ems_codes <emilyxi@gmail.com>
2020-06-01 17:28:10 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+attr_reader :word
+
+def initialize(word)
+    @word = word
+end
+
+def match(array)
+    match_word = @word.scan(/\w/)
+    return_array = []
+    array.each do |x| 
+        if match_word.sort == x.scan(/\w/).sort 
+            return_array << x
+        end
+    end
+    return_array
+end
+end
+
+test = ["emily","hello","whatsup"]
+myanagram = Anagram.new("elloh")
+myanagram.match(test)
