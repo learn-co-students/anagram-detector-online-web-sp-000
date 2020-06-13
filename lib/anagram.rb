32ac5ac3b9cc63e869016b9832940f05599e2f78
Justin Breneman <justin.breneman@yahoo.com>
2020-06-13 00:13:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select{|anagram| anagram.split("").sort == @word.split("").sort}
+  end
+end
