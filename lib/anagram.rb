077ca1cf69e80e4ee273ee4a188b559608168c24
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-27 11:12:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Anagram
+  
+  @empty_array = []
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    @word_array = []
+    array.each do |array_word|
+      split_word = array_word.split("")
+      split_anagram = @word.split("")
+      if split_word.sort == split_anagram.sort
+        @word_array << array_word
+      end
+      
+    end
+    @word_array
+end
+
+end
