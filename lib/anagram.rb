97c05290cc9561e4a799f2e31f9f0fb9389401c0
Rafael Garcia <rafagrcb@gmail.com>
2020-07-08 17:49:45 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array_word)
+    array_word.select do |element|
+      (word.split("").sort) == (element.split("").sort)
+    end
+  end
+  
+end
+
+
+      
