a9389c6882dd4e76c0a6afc3949516a256950e1a
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-28 19:30:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+    split_word = @word.split("").sort
+    words_array.select{|word| word.split("").sort == split_word}
+  end
+
+end
