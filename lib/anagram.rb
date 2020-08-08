64938061de50a093fe7bc6a8431fa41b8da8ba55
Nick Lanese <nlanese21@gmail.com>
2020-08-08 01:28:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  def initialize(targetWord)
+    @targetWord = targetWord
+  end
+
+  def match(wordArray)
+    rArray = []
+    @wordArray = wordArray
+    @wordArray.each do | word |
+      splitWord = word.split("")
+      splitTarget = @targetWord.split("")
+      splitWord = splitWord.sort
+      splitTarget = splitTarget.sort
+    #  binding.pry
+      if (splitTarget == splitWord)
+        rArray << word
+      end
+    end
+    return rArray
+  end
+
+end
