e3dcd01a56bea762218268cb881540398a3b039d
Anya Brun <tsbrun@icloud.com>
2020-08-11 19:04:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,15 +8,17 @@ class Anagram
   end
 
   def match(possible_matches)
-    matches = []
+    #matches = []
     word_letters = @word.split("").sort
 
-    possible_matches.each do |word|
-      if word.split("").sort == word_letters
-        matches << word
-      end
-    end
+    #possible_matches.each do |word|
+    #  if word.split("").sort == word_letters
+    #    matches << word
+    #  end
+    #end
 
-    matches
+    possible_matches.select { |word| word.split("").sort == word_letters }
+
+    #matches
   end
 end
