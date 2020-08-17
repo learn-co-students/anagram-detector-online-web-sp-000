fa5866f5b9974996954a4fecba9a57259fade238
jasonronalddavis <jdavis@c.ringling.edu>
2020-08-17 03:06:48 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+
+
+    attr_accessor :sentence_data
+
+    def initialize(sentence)
+   @sentence_data = sentence
+    end
+
+    def match(letters)
+        letters.select do |letter|
+            is_match?(letter)
+        end
+    end
+
+            def is_match?(letter)
+                letter.chars.sort == @sentence_data.chars.sort
+end
+end
+    
