0fed73151c9529658fd355b275d57796eed01c58
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-19 09:14:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+  matches = []
+  sortedword = @word.split("").sort
+
+  array.each do | scrambledword |
+    sortedletters = scrambledword.split("").sort
+    if sortedletters == sortedword
+      matches << scrambledword
+    end #end of if
+  end #endof iteration
+
+  return matches
+end
+
+end
+
+hey = Anagram.new("hello")
+puts hey.match(%w(enlists google inlets banana))
