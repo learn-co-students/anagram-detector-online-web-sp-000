16d5e3ee17f62677734bf0963050adf72e7f89fb
John Stassen <john.stassen@gmail.com>
2020-08-26 14:40:15 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+  def initialize(word)
+    anagram = self
+    @word = word
+  end
+
+  def match(array_of_possible_anagrams)
+    @solution = []
+    array_of_possible_anagrams.each do |possible|
+      possible_array = possible.split("")
+      word_array = @word.split("")
+      if possible_array.sort == word_array.sort
+        @solution << possible
+      else
+        @solution = @solution
+      end
+    end
+    return @solution
+  end
+end
