c5fec2f83df9fec54d906c788b3b6bdea49d3eeb
javonesm89 <javosmith12@gmail.com>
2020-09-16 11:45:16 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|w| w.split("").sort == self.word.split("").sort}
+    end
+
+end
+
+a = Anagram.new("live")
+a.match(%w(evil google plants vile irish))
