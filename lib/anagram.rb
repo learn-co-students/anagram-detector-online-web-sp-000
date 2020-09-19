6efaa18dfb4b130bde11e4ab898c5107b9b310f1
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-19 10:20:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def split_sort(word)
+    word.split("").sort
+  end
+  
+  def match(array)
+    array.select do |array_word|
+      split_sort(array_word) == split_sort(word)
+    end
+  end
+  
+end
