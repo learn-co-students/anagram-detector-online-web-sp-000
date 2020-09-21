a47d964fc4330335cea00f871bd856bbdf5d07b7
Jc Dior <jessicaclairdior@gmail.com>
2020-09-21 22:11:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(anagram_array)
+   anagram_array.select do |word|
+     word.chars.sort == @word.chars.sort
+end
+end
+
+end
