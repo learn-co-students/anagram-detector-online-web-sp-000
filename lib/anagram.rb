7b2142a78f632ca33020028c39e314258b724790
shacoca <shacoca@yahoo.com>
2020-09-25 10:14:56 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        anagrams = []
+        words.each do |word|
+            anagrams << word if (@word.chars.uniq.length == word.chars.uniq.length) && @word.chars.sort.uniq.all? { |char|
+                (@word.chars.count(char) == word.chars.count(char))}
+        end
+        anagrams
+   end
+
+end
