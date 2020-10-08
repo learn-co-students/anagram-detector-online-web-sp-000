b8aa33b90c217576afb904d3cf34438b45374659
Alan Tang <giabao_tang@yahoo.com>
2020-10-08 23:02:47 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+  attr_accessor :word 
+  def initialize(word)
+    @word = word
+  end
+  
+  def match (list_words)
+    
+    array = list_words.find_all do |element|
+               self.word.split("").sort == element.split("").sort
+    end
+   
+    match = array || []
+  end  
+      
+end
