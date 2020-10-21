79f421c3effb995912919916f424bec731efafce
fut0k0 <cccabell@icloud.com>
2020-10-21 16:14:00 -0700

    Passing lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+	attr_accessor :word
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(array)
+		array.filter{|item| item.split("").sort == @word.split("").sort}
+	end
+end
