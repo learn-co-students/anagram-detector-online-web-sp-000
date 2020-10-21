8a16e320addeb4996c0647de51c6b0e264815f21
Alla Kuzminova <alchik@gmail.com>
2020-10-21 14:23:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    newarr = words.select do |item|
+      anagram?(item)
+    end
+    newarr
+  end
+
+  def anagram?(anotherword)
+    localwordarr = word.split("")
+    anotherwordarr = anotherword.split("")
+    localwordarr.sort == anotherwordarr.sort
+  end
+end
