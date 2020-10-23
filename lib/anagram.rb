f1f391d210b4640c69d776417dc29292674c3663
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-23 03:54:16 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(string)
+    string.select {|word| word.chars.sort == @word.chars.sort}
+  end
+
+
+end
