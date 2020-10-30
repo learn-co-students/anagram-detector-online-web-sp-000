a9717bf8fa9edae7c7d2795a1e2a125e4ea46246
Damian Malysza <damian.malysza@gmail.com>
2020-10-30 01:50:36 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,13 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :word, :word_letters_sorted
+  attr_accessor :word
 
     def initialize(word)
       @word = word
-      @word_letters_sorted = word.split("").sort
     end
 
     def match(word_list)
-      word_list.select {|word| word.split("").sort == @word_letters_sorted}
+      word_list.select {|word| word.split("").sort == @word.split("").sort}
     end
 
 end
