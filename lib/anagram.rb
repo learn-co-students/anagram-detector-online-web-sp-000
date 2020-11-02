edcfdfae593601416d1d7c9017d61b68f6350e48
Will Studabaker <will.studabaker@gmail.com>
2020-11-02 12:27:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  def match(array)
+    #compares each word in array to @word
+    match_array = []
+    array.each do |word|
+      match_array << word if word.split("").sort == @word.split("").sort
+    end
+    match_array
+  end
+end
