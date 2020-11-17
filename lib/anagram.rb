7cf52212c86972c17d152b67be1f812fd9874afa
John Million <jpmillion@comcast.net>
2020-11-17 17:01:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(array)
+    array.select {|word| word.split('').sort == @word.split('').sort}
+  end
+end
