42b5e908dc129f214ae6dda9fd2f0a174a55ab89
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-29 18:57:27 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+      
+  @@anagrams = []
+  
+  def initialize(word)
+    @word = word
+    @@anagrams.clear
+  end
+  
+  def match(array)
+    array.each do |word|
+      @@anagrams << word if split(word) == split(@word)
+    end
+    @@anagrams
+  end
+  
+  private
+  
+  def split(word)
+    split_array = word.split (//)
+    split_array.sort
+  end
+  
+end
