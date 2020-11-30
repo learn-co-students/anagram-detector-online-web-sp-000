a9822128be377cbc2b972867a42e054e48dff959
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-30 05:07:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    @word = @word.split(//)
+    @word.sort!
+
+    @split_anagrams = []
+
+    @anagrams = anagrams
+    @anagrams.each { |word| @split_anagrams << word.split(//) }
+
+    @split_anagrams.each { |word| word.sort! }
+
+    @split_anagrams.find{|word| word == @word }
+  end
+end
