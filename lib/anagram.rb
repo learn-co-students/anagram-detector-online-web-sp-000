b2638d571637734dedeb35a17c08ab8639a21aeb
Michael Fuget <michaelfuget@gmail.com>
2020-12-03 02:02:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,50 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def self.word_to_letter_counts_map(word)
+    out = {}
+    word.split('').each do |letter|
+      if(!out.has_key?(letter))
+        out[letter] = 1;
+      else 
+        out[letter]+=1;
+      end 
+    end 
+    return out
+  end 
+  
+  def match(array)
+    word_char_counts = Anagram.word_to_letter_counts_map(@word)
+    out = []
+    array.each do |array_word|
+      is_match = true
+      array_word_char_counts = Anagram.word_to_letter_counts_map(array_word)
+      word_char_counts.each do |key, value|
+        if(array_word_char_counts[key] != value)
+          is_match = false 
+        end 
+      end 
+      
+      if(array_word.length != @word.length)
+        is_match = false 
+      end 
+      
+      if(is_match)
+        out.push(array_word)
+      end 
+    end
+    return out 
+  end 
+end
+      
+      
+      
+
+  
