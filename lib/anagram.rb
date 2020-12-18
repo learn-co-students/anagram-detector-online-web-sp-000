29f80c836f22ebd3bf325f07a654ab9d9fc517f2
Romano Livi <romano_livi@outlook.com>
2020-12-18 21:48:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    words.select do |w|
+      @word.split("").sort == w.split("").sort
+    end
+  end
+  
+end
