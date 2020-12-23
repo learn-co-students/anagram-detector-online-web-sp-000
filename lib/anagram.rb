6134b9c031409855968ebc40dd20c5131884d789
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-23 07:09:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :letters
+  
+  def initialize(letters)
+    
+    @letters = letters
+    
+  end
+  
+  def match(words_arr)
+    
+    anagrams = []
+    
+    words_arr.each do |word|
+      
+      if (word.split("").sort == @letters.split("").sort)
+        anagrams.push(word)
+      end
+
+      
+    end
+    
+    anagrams
+    
+  end
+  
+end
