72e69ef25a9e8fc77b4283aad759d29d776a2c05
Jordan Reed <jordanreed03@gmail.com>
2021-01-05 07:11:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(arr)
+    new_arr = [ ]
+    varletters = @word.split("").sort
+    
+    arr.each do |words|
+      letters = words.split("").sort
+      
+      if letters == varletters 
+        new_arr << words
+      end 
+    end
+    return new_arr
+  end 
+  
+end 
+
+def budg(a, b, onek)
+  return var = onek - ( a + b )
+end 
