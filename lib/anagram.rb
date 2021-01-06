5907e84808386f54ad28afaf1c6f31c9203d3739
Danny Brandt <dvbrandt90@gmail.com>
2021-01-06 23:17:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+      @word = word.split("")
+  end
+  def match(word_array)
+    words = word_array.map do |word|
+      letters = word.split('')
+    a = letters.sort
+    b = @word.sort
+    if  a == b 
+      word 
+    end
+    end
+    return words.compact
+  end
+end
