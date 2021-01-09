8cbbd9c7d84298c45ea0883955e9ccd1b9edd949
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-09 20:14:00 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+def splitsort
+  @word.downcase.split(//).sort
+end
+
+  def match(anagram)
+    matches = []
+    anagram.each do |element|
+      split = element.downcase.split(//)
+        if split.sort == splitsort
+          matches << element
+        end
+      end
+    matches
+    end
+end
