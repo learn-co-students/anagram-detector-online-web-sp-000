d0f7cd0acd46fd5bbd4eec32301e8ad9f41a7470
Jouse then <jthen93@gmail.com>
2021-02-24 21:50:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match (anagram)
+    anagram_list = []
+    sorted_word = @word.split(//).sort!.join
+    
+    anagram.each do |w|
+      sorted_w = w.split(//).sort.join
+      if sorted_w == sorted_word
+        anagram_list << w
+      end
+    end
+    
+    return anagram_list
+  end
+end
