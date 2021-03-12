88e10f35345afb5535a79da649c0a850014159fb
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-12 00:46:14 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagrams.select do |word|
+      word.chars.sort == @word.chars.sort
+    end
+  end
+
+end
