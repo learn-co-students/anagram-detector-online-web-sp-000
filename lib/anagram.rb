c940c15f07076c03f7ce680a52b74c6d60cb1972
Clayon Stewart <c.stewart89@gmail.com>
2021-03-12 16:01:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :match
+  
+  def initialize(subject)
+    @match = subject
+  end
+  
+  def match(match_cases)
+    
+    same_letters = []
+    match_cases.each do |word|
+      if (word.length == @match.length) && (word.split("").sort == @match.split("").sort)
+        same_letters << word
+      end
+    end
+    
+    same_letters
+  end
+end    
