406732aeaf3e7cfe9df460aca0f174d99e06788c
Drew Keat <thedrewkeat@gmail.com>
2021-04-19 23:19:04 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,8 +11,18 @@ class Anagram
         word_arr = @word.split("").sort
         array.each do |word|
             checker = word.split("").sort
-            checker == word_arr ? final << word : final = final
+            count = 0
+            checker.each_with_index do |char, i|
+                if checker[i] == word_arr[i]
+                    count += 1
+                    # binding.pry
+                else 
+                    count -= 1
+                end
+            end
+            count == word_arr.size ? final << word : final = final
         end
+        final
     end
     
 end
