c1e4f9bc0a8198d76741e2cf3a2e58cf7a36c8ee
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-21 22:01:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(test_words) # test_words is an array
+    sorted_word_letters = @word.split("").sort{|a,b|a<=>b}
+    
+    test_words.select do |test_word|
+      sorted_test_word_letters = test_word.split("").sort{|a,b|a<=>b}
+      sorted_test_word_letters == sorted_word_letters 
+    end 
+  end 
+  
+end 
