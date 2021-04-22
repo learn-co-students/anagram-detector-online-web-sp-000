3f8685edf504f33d0ea129671a37749807810a08
Becky Rosa <becky_m_rosa@yahoo.com>
2021-04-22 03:17:34 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def broken_word
+        @word.split("").sort
+    end
+    def broken_array(word)
+        word.split("").sort
+    end
+    def match(array)
+       array.select do |word|
+        broken_array(word) == broken_word
+       end
+    end
+end
