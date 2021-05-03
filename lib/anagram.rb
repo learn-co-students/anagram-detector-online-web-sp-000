ad84dee813ef931b39e5d41018ac6fe08a974c43
rzemlin <34727847+rzemlin@users.noreply.github.com>
2021-05-03 13:41:05 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,13 @@
-class anagram
+class Anagram
 
-    attr_accessor :word
-end
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|word| word.split("").sort == @word.split("").sort}
+    end
 
 end
