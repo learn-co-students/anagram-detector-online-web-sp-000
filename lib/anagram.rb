3375f6c5f4a2d38d4b3d41d852a869c061afc252
Guosa_Edebiri <gedebiri@gmail.com>
2021-05-24 07:45:24 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_array)
+
+        anagram_array.select do |anagram|
+            anagram.split("").sort == @word.split("").sort
+        end
+
+    end
+
+end
+
