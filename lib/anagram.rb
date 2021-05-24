7f32ca8ad7ef8cf451f160f1cb98060e33350db0
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-24 16:27:57 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    matches = []
+    sorted_word = @word.chars.sort.join
+    array.each do |word|
+      sorted_letters = word.chars.sort.join
+      if sorted_letters == sorted_word
+        matches << word
+      end
+    end
+    matches
+  end
+
+end
