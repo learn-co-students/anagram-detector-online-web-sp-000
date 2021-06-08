a3ec41a3536011d80d7d0f1b1b54001ca8d2f78b
DJ Taylor <dtaylor88@gmail.com>
2021-06-08 02:41:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(array)
+   array.select{|e| e.split("").sort.join == @word.split("").sort.join}
+  end 
+  
+end 
