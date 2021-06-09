4d8be7111f14a65830ad77b7c0b2a8c04d57754d
James Tillman <james@itme-music.com>
2021-06-09 20:40:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+#creates getter & setter for the inputted word (thing comparing against)
+  attr_accessor :word
+
+#on initialize set a class instance variable
+  def initialize(word)
+    @word = word
+  end
+
+#take in the array of words, with select (returns only the values == true)
+#iterate over the array of words (w) split per charcter, sort them and compare
+#to the anagram which will be split and sorted in the same way
+  def match(array_of_words)
+    array_of_words.select {|w| w.split("").sort == @word.split("").sort}
+  end
+
+end
